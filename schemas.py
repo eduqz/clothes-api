8e2a9653448e34b643f8f4733e7dbc61461c3797
eduardoaqueiroz <eaq@cin.ufpe.br>
2020-10-12 14:25:42 -0300

    create schemas

---
+++ schemas.py
@@ -0,0 +1,40 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class ClothesBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+    size: Optional[str] = None
+    color: Optional[str] = None
+
+
+class ClothesCreate(ClothesBase):
+    category_id: int
+
+
+class Clothes(ClothesBase):
+    id: int
+    category_id: int
+
+
+    class Config:
+        orm_mode = True
+
+
+class CategoryBase(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
+class CategoryCreate(CategoryBase):
+    pass
+
+
+class Category(CategoryBase):
+    id: int
+    items: List[Clothes] = []
+
+    class Config:
+        orm_mode = True
