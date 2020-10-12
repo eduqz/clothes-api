8f0a1949d118d23e498fd2eebfb44474f4b7ae58
eduardoaqueiroz <eaq@cin.ufpe.br>
2020-10-12 12:27:10 -0300

    create models

---
+++ models.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Clothes(Base):
+    __tablename__ = "clothes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String)
+    size = Column(String)
+    color = Column(String)
+    category_id = Column(Integer, ForeignKey("category.id"))
+
+    category = relationship("Category", back_populates="items")
+
+
+class Category(Base):
+    __tablename__ = "category"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+
+    items = relationship("Clothes", back_populates="category")
